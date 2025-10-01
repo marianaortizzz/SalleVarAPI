8ab02c42d3f647b2fe9deab65a11ec9e929e9e62
josaPlus <josafat.saldana@hotmial.comm>
2025-10-01 01:40:06 -0600

    implementar routers cliente y negocio a main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from config.database import Session, engine, Base
+from routers.cliente import cliente_router
+from routers.negocio import negocio_router
 
 app= FastAPI()
 app.title="La poderosisima API de SalleVar"
@@ -8,6 +10,8 @@ app.version="0.0.1"
 
 Base.metadata.create_all(bind=engine)
 
+app.include_router(cliente_router)
+app.include_router(negocio_router)
 
 @app.get("/", tags=["Home"])
 def read_root():
