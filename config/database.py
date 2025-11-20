a5b62e3753d04f1e1beee5ec347c843992b99b49
80443 - Mariana Ortiz Zermeño <moz80443@lasallebajio.edu.mx>
2025-11-20 13:36:33 -0600

    Eliminar conexion a default database

--- config/database.py
+++ config/database.py
@@ -4,11 +4,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DEFAULT_DATABASE_URL = "mysql://root:root@127.0.0.1:3306/sallevar"
 
 SQLALCHEMY_DATABASE_URL_FULL = os.environ.get(
-    "DATABASE_URL",
-    DEFAULT_DATABASE_URL
+    "DATABASE_URL"
 )
 
 parsed_url = urlparse(SQLALCHEMY_DATABASE_URL_FULL)
