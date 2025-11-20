b9f8724d82a864b1d9348c5a9f0f0371cb74011a
80443 - Mariana Ortiz Zermeño <moz80443@lasallebajio.edu.mx>
2025-11-20 11:03:26 -0600

    Parseado de la database_url para limpiar ssl_mode

--- config/database.py
+++ config/database.py
@@ -1,18 +1,22 @@
 import os
+from urllib.parse import urlparse, urlunparse
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 DEFAULT_DATABASE_URL = "mysql+pymysql://root:root@127.0.0.1:3306/sallevar"
 
-SQLALCHEMY_DATABASE_URL = os.environ.get(
+SQLALCHEMY_DATABASE_URL_FULL = os.environ.get(
     "DATABASE_URL",
     DEFAULT_DATABASE_URL
 )
 
+parsed_url = urlparse(SQLALCHEMY_DATABASE_URL_FULL)
+SQLALCHEMY_DATABASE_URL = urlunparse(parsed_url._replace(query=''))
+
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, 
-    echo=True, 
+    echo=True,
     connect_args={
         'ssl': {
             'ssl_mode': 'REQUIRED'
